117b5ec6e3603b9d737213f2d7def871990814cc
OliverB <pilu@Pilus-MacBook-Pro.local>
2019-01-22 12:02:40 +0100

    Basic server, react settings

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+
+
+
+export default class App extends React.Component {
+    constructor() {
+        super();
+    }
+
+
+
+    render() {
+        return(
+
+            <div>Hello</div>
+            
+        );
+    }
+}
