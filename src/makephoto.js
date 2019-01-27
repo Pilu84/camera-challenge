71180a00d05624d37187f2927682f426ee43c9dc
OliverB <pilu@Pilus-MacBook-Pro.local>
2019-01-28 00:39:47 +0100

    Change camera code

--- src/makephoto.js
+++ src/makephoto.js
@@ -1,34 +1,75 @@
-import React, { Component } from 'react';
-import Camera from 'react-camera';
+import React from 'react';
 
-export default class MakePhoto extends Component {
+
+
+export default class MakePhoto extends React.Component {
 
     constructor(props) {
         super(props);
         this.takePicture = this.takePicture.bind(this);
+        this.state = {picture: "", pictureToInput: props.pictureToInput};
     }
 
-    // takePicture() {
-    //
-    //     const cameraView = this.camera.video,
-    //         cameraOutput = this.img,
-    //         cameraSensor = document.querySelector("#camera--sensor");
-    //
-    //
-    //
-    //     cameraSensor.width = cameraView.videoWidth;
-    //     cameraSensor.height = cameraView.videoHeight;
-    //     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    //     cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    //
-    //
-    // }
+
 
     takePicture() {
-        this.camera.capture()
-            .then(blob => {
-                this.img.src = URL.createObjectURL(blob);
-                this.img.onload = () => { URL.revokeObjectURL(this.src); };
+
+        const cameraView = document.querySelector("video"),
+            cameraOutput = this.img,
+            cameraSensor = this.canv;
+
+
+        cameraSensor.width = cameraView.videoWidth;
+        cameraSensor.height = cameraView.videoHeight;
+        cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+        cameraOutput.src = cameraSensor.toDataURL("image/webp");
+
+
+        this.setState({picture: cameraOutput});
+
+        this.state.pictureToInput(cameraOutput.src);
+
+    }
+
+
+    componentDidMount() {
+
+        if (navigator.mediaDevices === undefined) {
+            navigator.mediaDevices = {};
+        }
+
+        if (navigator.mediaDevices.getUserMedia === undefined) {
+            navigator.mediaDevices.getUserMedia = function(constraints) {
+
+
+                var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+                if (!getUserMedia) {
+                    return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+                }
+
+                return new Promise(function(resolve, reject) {
+                    getUserMedia.call(navigator, constraints, resolve, reject);
+                });
+            };
+        }
+
+        navigator.mediaDevices.getUserMedia({ audio: false, video: { facingMode: "environment" } })
+            .then(function(stream) {
+                var video = document.querySelector('video');
+
+                if ("srcObject" in video) {
+                    video.srcObject = stream;
+                } else {
+
+                    video.src = window.URL.createObjectURL(stream);
+                }
+                video.onloadedmetadata = function() {
+                    video.play();
+                };
+            })
+            .catch(function(err) {
+                console.log(err.name + ": " + err.message);
             });
     }
 
@@ -37,31 +78,18 @@ export default class MakePhoto extends Component {
 
 
         return (
-            <div className="fluid">
+            <div className="input-group mb-3">
 
-
-                <Camera
-                    className="preview"
-                    ref={(cam) => {
-                        this.camera = cam;
-                    }}
-                >
-                    <div className="captureContainer" onClick={this.takePicture}>
-                        <div className="captureButton" />
-                    </div>
-                </Camera>
-
-                <div className="row">
-                    <img
-                        className ="captureImage"
-
-                        ref={(img) => {
-                            this.img = img;
-                        }}
-                    />
-
-                    <canvas id="camera--sensor" className="fluid"></canvas>
+                <canvas ref={(canv) => {this.canv = canv; }} className="non-visible"></canvas>
+                <div className="row mb-5">
+                    <img className ="w-100 h-100" ref={(img) => {this.img = img;}} />
                 </div>
+                <video className="w-100 h-100" ref={(camera) => {this.camera = camera;}}></video>
+
+                <div className="captureContainer" onClick={this.takePicture}>
+                    <div className="captureButton"></div>
+                </div>
+
             </div>
         );
     }
